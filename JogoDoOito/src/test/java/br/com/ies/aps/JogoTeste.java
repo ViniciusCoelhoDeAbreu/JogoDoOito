894a69fddc34faab2f0e0be4dc3e959f4ca7f7cf
Vinicius Coelho de Abreu <kickpostgameplays@gmail.com>
2020-10-19 18:37:00 -0300

    Iniciado teste e JUnit.

---
+++ JogoDoOito/src/test/java/br/com/ies/aps/JogoTeste.java
@@ -0,0 +1,24 @@
+package br.com.ies.aps;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.ies.aps.manager.GameManager;
+
+public class JogoTeste {
+
+	private GameManager gameManager;
+	
+	@Before
+	public void init() {
+	}
+	
+	@Test
+	public void testa() {
+		assertEquals(0, 1);
+	}
+	
+	
+}
